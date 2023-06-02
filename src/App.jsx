fc0cf63924e25544b4c57aa9d565bf5686ea5ca1
Yann33l <111899082+Yann33l@users.noreply.github.com>
2023-06-02 18:43:24 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+function App() {
+  return (
+    <main class="Connexion" >
+        <div class="centered-element">
+          <h1>Log in</h1>
+          <p >Besoin d'un compte? <a href=""> Cliquer içi</a></p>
+          <h3>Email <br />
+            <input type="text" placeholder="Login" />
+          </h3>
+          <h3>Password<br />
+            <input type="password" placeholder="Mot de passe" />
+          </h3>
+          <button >Connexion</button>  
+        </div>
+    </main>
+
+  )
+}
+
+export default App;
---
+++ src/on.jsx
@@ -0,0 +1,6 @@
+export function Conneon() {
+    return (
+
+    );
+}
+
