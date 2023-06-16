19f0021986e36db32cf3b84d293e2e3a5ee436ff
Yann33l <111899082+Yann33l@users.noreply.github.com>
2023-06-16 11:17:54 +0200

    16/06/2023

--- src/App.jsx
+++ src/App.jsx
@@ -1,53 +1,51 @@
 import React, { useState } from 'react';
 import HomePage from './homepage';
 import HomePageAdmin from './HomePageAdmin';
+import { checkCredentials } from './api';
+
+function checkAdmin(Email) {
+  return Email === 'admin@admin';
+}
+
 
 function App() {
   const [loggedIn, setLoggedIn] = useState(false);
-  const [loading, setLoading] = useState(false);
 
   async function handleLogin(event) {
     event.preventDefault(); // Empêcher le comportement par défaut du formulaire
 
-    const email = event.target.elements.email.value;
-    const password = event.target.elements.password.value;
+    const Email = event.target.elements.Email.value;
+    const Password = event.target.elements.Password.value;
 
-    setLoading(true); // Activer l'indicateur de chargement
-
-    if (email === 'test@test' || 'admin@admin' && password === '1234') {
-      // Vérifier si l'utilisateur est un administrateur
-      const isAdmin = checkAdmin(email);
-
-      if (isAdmin) {
+    if ((Email === 'test@test' || Email === 'admin@admin') && Password === '1234') {
+      // Connection de base pour test acces page User vs Admin
+      if ( checkAdmin(Email)) {
         setLoggedIn('admin');
       } else {
         setLoggedIn(true);
       }
-
-      setLoading(false); // Désactiver l'indicateur de chargement
     } else {
+      // Connexion via SGBD
       try {
-        const response = await checkCredentials(email, password);
-
+        const data = await checkCredentials(Email, Password);
         if (response.success) {
-          setLoggedIn(true);
+          if (data.isAdmin = true) {
+            setLoggedIn('admin')
+            ;
+          } else {
+            setLoggedIn(true);
+          };
         } else {
-          alert(response.message);
+          alert(data.message);
         }
       } catch (error) {
         alert('Une erreur s\'est produite lors de la vérification des informations d\'identification.');
       } finally {
-        setLoading(false); // Désactiver l'indicateur de chargement
       }
     }
   }
 
-  function checkAdmin(email) {
-  // Vérifier si l'utilisateur est un administrateur
-    // Implémentez votre logique de vérification des administrateurs ici
-    // Retournez true si l'utilisateur est un administrateur, sinon false
-    return email === 'admin@admin';
-  }
+ 
 
   if (loggedIn === 'admin') {
     // Afficher la vue pour les administrateurs
@@ -61,14 +59,13 @@ function App() {
         <h1>Log in</h1>
         <br />
         <form onSubmit={handleLogin}>
-          <label htmlFor="email">Email<br /></label>
-          <input name="email" type="email" placeholder="Email" />
+          <label htmlFor="Email">Email<br /></label>
+          <input name="Email" type="Email" placeholder="Email" />
           <br /><br />
-          <label htmlFor="password">Password<br /></label>
-          <input name="password" type="password" placeholder="Mot de passe" />
+          <label htmlFor="Password">Password<br /></label>
+          <input name="Password" type="Password" placeholder="Mot de passe" />
           <br /><br />
-          <button type="submit" disabled={loading}>
-            {loading ? 'Connexion en cours...' : 'Connexion'}
+          <button type="submit"> Connexion
           </button>
         </form>
         <p>Besoin d'un compte? <a href="#">Cliquer ici</a></p>
--- src/api.js
+++ src/api.js
@@ -1,14 +1,22 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000'; // Remplacez par l'URL de votre API
+const API_URL = 'http://0.0.0.0:8000'; // Remplacez par l'URL de votre API
 
-export const checkCredentials = async (email, password) => {
+export const checkCredentials = async (Email, Password) => {
   try {
-    const response = await axios.post(`${API_URL}/login`, { email, password });
-    return response.data;
+    const response = await axios.post(`${API_URL}/users/`, { Email, Password });
+    const data = response.data;
+
+    // Vérification de la colonne "Admin" dans la réponse de l'API
+    if (data.admin === 1) {
+      data.isAdmin = true;
+    } else {
+      data.isAdmin = false;
+    }
+
+    return data;
   } catch (error) {
     console.error('Une erreur s\'est produite lors de la vérification des informations d\'identification :', error);
     throw error;
   }
 };
-
