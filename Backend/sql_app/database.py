7377691e489049c3d41a7fe521d5f7fb12860b48
Yann33l <111899082+Yann33l@users.noreply.github.com>
2023-10-10 20:00:54 +0200

    probleme usessl

--- Backend/sql_app/database.py
+++ Backend/sql_app/database.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
 # scalingo
-SCALINGO_MYSQL_URL= "mysql://goldenline_7195:MLysrp1Px4WZhs0ZEJAV@94e79af0-02d8-4af3-862a-4516a9e45f09.goldenline-7195.mysql.a.osc-fr1.scalingo-dbs.com:30567/goldenline_7195?useSSL=true&verifyServerCertificate=false"
+SCALINGO_MYSQL_URL= "mysql://goldenline_7195:MLysrp1Px4WZhs0ZEJAV@94e79af0-02d8-4af3-862a-4516a9e45f09.goldenline-7195.mysql.a.osc-fr1.scalingo-dbs.com:30567/goldenline_7195"
 """ DATABASE_URL=$SCALINGO_MYSQL_URL """
 
 engine = create_engine(SCALINGO_MYSQL_URL)
