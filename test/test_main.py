9bea8853e11569107dc93207cdc316b9204714c1
Yann33l <111899082+Yann33l@users.noreply.github.com>
2023-10-16 11:37:48 +0200

    test main et test CRUD

--- test/test_main.py
+++ test/test_main.py
@@ -3,7 +3,7 @@ from unittest.mock import Mock
 import bcrypt
 from fastapi.testclient import TestClient
 
-from Backend.sql_app import models, schemas, CRUD
+from Backend.sql_app import CRUD, models, schemas
 from Backend.sql_app.database import engine
 from main import app
 
@@ -13,9 +13,11 @@ client = TestClient(app)
 
 # arrange
 db = Mock()
+salt = bcrypt.gensalt(12)
+password_byte = b"password"
 user = schemas.UserCreate(
     Email="test@test.com",
-    Password=b"password",
+    Password=bcrypt.hashpw(password_byte, salt),
     Admin=False,
     Autorisation=True
 )
@@ -33,7 +35,6 @@ def test_create_user_doesnt_exist():
     assert response.json()["Email"] == user.Email
     assert response.json()["Admin"] == user.Admin
 
-
 def test_create_user_already_exists():
 
     CRUD.get_user_by_email = Mock(return_value=True)
@@ -43,8 +44,7 @@ def test_create_user_already_exists():
     assert response.status_code == 400
     assert response.json()["detail"] == "Email already registered"
 
-
-def test_read_users():
+def test_read_users_exist():
 
     CRUD.get_users = Mock(return_value=[user, user])
 
@@ -56,43 +56,51 @@ def test_read_users():
     assert response.json()[1]["Email"] == user.Email
     assert response.json()[1]["Admin"] == user.Admin
 
+def test_read_user_by_ID():
 
-def test_read_user():
-    db_user = models.users(**user.dict())
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    response = client.get(f"/users/{db_user.id}")
+    CRUD.get_user_by_ID = Mock(return_value=user)
+
+    response = client.get("/users/2")
     assert response.status_code == 200
     assert response.json()["Email"] == user.Email
     assert response.json()["Admin"] == user.Admin
 
-def test_read_user_not_found():
-    response = client.get("/users/999")
+def test_read_user_by_ID_not_found():
+    
+    db_user = None
+    CRUD.get_user_by_ID = Mock(return_value=db_user)
+    response = client.get("/users/55")
+
     assert response.status_code == 404
     assert response.json()["detail"] == "User not found"
 
+
 def test_connexion():
-    db_user = models.users(**user.dict())
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
-    response = client.get(f"/Connexion/?email={user.Email}&password={user.Password}")
+
+    CRUD.get_user_by_email = Mock(return_value=user)
+
+    response = client.get(f"/Connexion/?email={user.Email}&password=password")
+    
     assert response.status_code == 200
     assert response.json()["Email"] == user.Email
     assert response.json()["Admin"] == user.Admin
+    assert response.json()["Autorisation"] == user.Autorisation
 
 def test_connexion_incorrect_email():
+
+    db_user = None
+    CRUD.get_user_by_email = Mock(return_value=db_user)
+
     response = client.get("/Connexion/?email=incorrect@test.com&password=password")
+
     assert response.status_code == 404
     assert response.json()["detail"] == "Email incorrect"
 
 def test_connexion_incorrect_password():
-    db_user = models.users(**user.dict())
-    db.add(db_user)
-    db.commit()
-    db.refresh(db_user)
+
+    CRUD.get_user_by_email = Mock(return_value=user)
     response = client.get("/Connexion/?email=test@test.com&password=incorrect")
+
     assert response.status_code == 404
     assert response.json()["detail"] == "Mot de passe incorrect"
 
